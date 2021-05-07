70780eeaf502da1229ea75fcc96e956c99a000e2
rkbrian <rkbriankong@gmail.com>
2021-05-07 23:20:47 +0000

    progress

--- pi/pi_calc.c
+++ pi/pi_calc.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- *
- *
- *
+ * pi_calc - function to calculate pi
  */
+int pi_calc(int a)
+{
+	if (n == a)
+	{
+		pi_calc();
+	}
+	return (float(pi));
+}
+
+/**
+ * main - print pi with decimal place of i
+ */
+int main(int argc, char *argv[])
+{
+	int i = atoi(argv[1]);
+	float pi;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	pi = pi_calc(i)
+	printf("%d\n", pi);
+	return (0);
+}
---
+++ pi/quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+	if life() == journey()
+	{
+		life(pi) = journey(decimal place);
+	}
+	else
+		return (home);
+}
