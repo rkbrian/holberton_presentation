ce373577587bd81060da199fef1a1642d47de5db
rkbrian <rkbriankong@gmail.com>
2021-05-29 08:29:45 +0000

    final?

--- pi/pi_calc.c
+++ pi/pi_calc.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+int main(int argc, char *argv[])
 {
-  int a = 300;
+  int a = atoi(argv[1]);
   float n = 0.0;
   float i;
   float j = -1.0;
 
+  if (argc != 2)
+    {
+      printf("Error: please provide a valid number\n");
+      return (1);
+    }
   for (i = 0.0; i < a; i++)
     {
       j = j * (-1);
       n = n + (float)(4 * j / (float)(2 * i + 1));
     }
-  printf("The pi approximation after sum of %d fractions is %0.8f\n", a, n);
+  printf("My pi approximation after sum of %d fractions is %0.8f\n", a, n);
+  printf("Is my result anywhere close to the real pi value of 3.141592653589793... ?\n");
   return (0);
 }
